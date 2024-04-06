e4e23154bb35e2eaa36dfaf559aaaa0c5c1e18f6
Geoff Bourne <itzgeoff@gmail.com>
2024-04-05 22:33:39 -0500

    Only output last error of retries

--- java_status.go
+++ java_status.go
@@ -106,7 +106,11 @@ func (c *statusCmd) Execute(_ context.Context, _ *flag.FlagSet, args ...interfac
 
 		return nil
 
-	}, retry.Delay(c.RetryInterval), retry.DelayType(retry.FixedDelay), retry.Attempts(uint(c.RetryLimit+1)))
+	},
+		retry.Delay(c.RetryInterval),
+		retry.DelayType(retry.FixedDelay),
+		retry.Attempts(uint(c.RetryLimit+1)),
+		retry.LastErrorOnly(true))
 
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "failed to ping %s:%d : %s", c.Host, c.Port, err)
